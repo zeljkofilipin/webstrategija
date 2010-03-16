ab168d2f64177115180cfbbb97711fb7c6142ecd
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-03-16 22:54:20 +0100

    deleted all google search code

--- rspec.rb
+++ rspec.rb
@@ -3,15 +3,4 @@ describe "Watir" do
 		require "watir-webdriver"
     @browser = Watir::Browser.new :ff
   end
-
-  it "should open google.hr when asked to open google.com" do
-    @browser.goto "google.com"
-		@browser.url.should == "http://www.google.hr/"
-  end
-  it "should should find Web::Strategija site when searching for Web Strategija" do
-    @browser.text_field(:name => "q").set "WebStrategija"
-		@browser.button(:name => "btnG").click
-		@browser.text.include?("Web::Strategija").should == true
-  end
 end
-
