dfbaab3b4a45028ecc69fc36759bfb659cb407e4
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-03-16 22:10:19 +0100

    rspec file generated by netbeans

---
+++ rspec.rb
@@ -0,0 +1,15 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'webstrategija'
+
+describe Webstrategija do
+  before(:each) do
+    @webstrategija = Webstrategija.new
+  end
+
+  it "should desc" do
+    # TODO
+  end
+end
+
