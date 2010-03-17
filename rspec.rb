38d2334c6aec5d6699b2ccdd53aed69a53cb1de0
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-03-17 22:05:39 +0100

    added link to Watir web site

--- rspec.rb
+++ rspec.rb
@@ -3,4 +3,9 @@ describe "Watir" do
 		require "watir-webdriver"
     @browser = Watir::Browser.new :ff
   end
+	it "should open www.webstrategija.com when navigating to webstrategija.com" do
+		@browser.goto "webstrategija.com"
+		@browser.url.should == "http://www.webstrategija.com/"
+	end
+	it "should open when 'Kontakt' link is clicked"
 end
