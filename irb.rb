3a66f1c5db93c84bcfd6de862f4c2985e97f7f73
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-03-17 22:17:00 +0100

    I had to create a new irb example, since the old one was using a page that is no longer with us

--- irb.rb
+++ irb.rb
@@ -1,49 +1,37 @@
-# $ irb
+#$ irb
 
 require "watir-webdriver"
 # => true
 
-b = Watir::Browser.new :chrome
-# => #<Watir::Browser:0x1016097b0 url="" title="">
+b = Watir::Browser.new :ff
+# => #<Watir::Browser:0x101682818 url="about:blank" title="">
 
-b.goto "webstrategija.com"
-# => "http://www.webstrategija.com/"
+b.goto "github.com"
+# => "http://github.com/"
 
-b.link(:text => /PRIJAVITE SE/).flash
+b.text_field(:index => 0).flash
 # => 10
 
-b.link(:text => /PRIJAVITE SE/).click
-# => []
+b.text_field(:index => 0).id
+# => ""
 
-b.radio(:name => "data[type]").flash
+b.text_field(:index => 0).name
+# => "q"
+
+b.text_field(:name => "q").flash
 # => 10
 
-b.radio(:name => "data[type]", :value => "1").flash
+b.text_field(:name => "q").set "watir"
+#=> ["watir"]
+
+b.button(:alt => "search").flash
 # => 10
 
-b.radio(:name => "data[type]", :value => "1").click
-# => []
-
-b.select_list(:id => "status").flash
-# => 10
-
-b.select_list(:id => "status").select "Novinar"
-# => "Novinar"
-
-b.text_field(:name => "data[name]").set "test"
-# => ["test"]
-
-b.button(:value => "Odustani").flash
-# => 10
-
-b.button(:value => "Odustani").click
+b.button(:alt => "search").click
 # => []
 
 b.url
-# => "http://www.webstrategija.com/07/prijava"
+# => "http://github.com/search?q=watir&x=0&y=0"
 
-b.text.include? "Sudionik"
+b.text.include? "bret"
 # => true
-
-#b.text.include? "Slon"
-# => false
