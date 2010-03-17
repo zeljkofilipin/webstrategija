98cf41970c3e7a2a4e3aea02449565163587b4da
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-03-17 22:29:52 +0100

    it should open Github home page when asked for github.com

--- rspec.rb
+++ rspec.rb
@@ -3,4 +3,8 @@ describe "Watir" do
 		require "watir-webdriver"
     @browser = Watir::Browser.new :ff
   end
+	it "should open Github home page when asked for github.com" do
+		@browser.goto "github.com"
+		@browser.url.should == "http://github.com/"
+	end
 end
