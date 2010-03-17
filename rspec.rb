f3ada29babe6b7e5ca4d7f9ffe96febda064fb89
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-03-17 22:31:05 +0100

    it should find Bret's repository when searching for Watir

--- rspec.rb
+++ rspec.rb
@@ -7,4 +7,13 @@ describe "Watir" do
 		@browser.goto "github.com"
 		@browser.url.should == "http://github.com/"
 	end
+	it "should find Bret's repository when searching for Watir" do
+		@browser.text_field(:name => "q").flash
+		@browser.text_field(:name => "q").set "watir"
+
+		@browser.button(:alt => "search").flash
+		@browser.button(:alt => "search").click
+
+		@browser.text.include?("bret").should == true
+	end
 end
