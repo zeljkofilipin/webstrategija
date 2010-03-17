fba48d5e21bf859f30888ae0249110d6d0fa7311
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-03-17 22:57:24 +0100

    smoke and mirrors

--- rspec.rb
+++ rspec.rb
@@ -8,12 +8,27 @@ describe "Watir" do
 		@browser.url.should == "http://github.com/"
 	end
 	it "should find Bret's repository when searching for Watir" do
-		@browser.text_field(:name => "q").flash
+		10.times {@browser.text_field(:name => "q").flash}
 		@browser.text_field(:name => "q").set "watir"
 
-		@browser.button(:alt => "search").flash
+		10.times {@browser.button(:alt => "search").flash}
 		@browser.button(:alt => "search").click
 
 		@browser.text.include?("bret").should == true
+	end
+	it "should do some smoke and mirrors" do
+		10.times {@browser.select_list(:name => "type").flash}
+		@browser.select_list(:name => "type").select "Code"
+
+		10.times {@browser.select_list(:name => "language").flash}
+		@browser.select_list(:name => "language").select "Ruby"
+
+		10.times {@browser.text_field(:name => "q", :index => 1).flash}
+		@browser.text_field(:name => "q", :index => 1).set "watir"
+
+		10.times {@browser.button(:alt => "search").flash}
+
+		10.times {@browser.link(:text => "bret / watir").flash}
+		@browser.link(:text => "bret / watir").click
 	end
 end
